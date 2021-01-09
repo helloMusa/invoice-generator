a984a562fbb840c1e82dcf8053d490d0be1b1703
Musa Ali <musa6ali@gmail.com>
2021-01-09 01:45:11 -0500

    Add tests to test PUT and DELETE

--- src/api/users.py
+++ src/api/users.py
@@ -18,14 +18,9 @@ user = api.model(
 )
 
 
-class Users(Resource):
+class UsersList(Resource):
     @api.marshal_with(user, as_list=True)
-    def get(self, user_id=None):
-        if user_id:
-            user = User.query.filter_by(id=user_id).first()
-            if not user:
-                api.abort(404, f"User {user_id} does not exist")
-            return user, 200
+    def get(self):
         return User.query.all(), 200
 
     @api.expect(user, validate=True)
@@ -47,5 +42,45 @@ class Users(Resource):
         return response_object, 201
 
 
-api.add_resource(Users, "/users")
+class Users(Resource):
+    @api.marshal_with(user)
+    def get(self, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        return user, 200
+
+    @api.expect(user, validate=True)
+    def put(self, user_id):
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+
+        user.username = username
+        user.email = email
+        db.session.commit()
+
+        response_object["message"] = f"{user.id} was updated!"
+        return response_object, 200
+
+    def delete(self, user_id):
+        response_object = {}
+        user = User.query.filter_by(id=user_id).first()
+
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+
+        db.session.delete(user)
+        db.session.commit()
+
+        response_object["message"] = f"{user.email} was removed!"
+        return response_object, 200
+
+
+api.add_resource(UsersList, "/users")
 api.add_resource(Users, "/users/<int:user_id>")
