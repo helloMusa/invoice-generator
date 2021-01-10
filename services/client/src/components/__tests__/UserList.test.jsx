6b2171e2c4279bfc813268cc9a8d6bb6d1f9533e
Musa Ali <musa6ali@gmail.com>
2021-01-09 20:45:23 -0500

    Set up React

---
+++ services/client/src/components/__tests__/UserList.test.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+
+import UsersList from "../UserList";
+
+afterEach(cleanup);
+
+const users = [
+  {
+    email: "musa6ali@gmail.com",
+    id: 1,
+    username: "musa",
+  },
+  {
+    email: "musa@mali.org",
+    id: 2,
+    username: "musaali",
+  },
+];
+
+it("renders a username", () => {
+  const { getByText } = render(<UsersList users={users} />);
+  expect(getByText("musa")).toHaveClass("username");
+  expect(getByText("musa")).toHaveClass("username");
+});
+
+it("renders", () => {
+  const { asFragment } = render(<UsersList users={users} />);
+  expect(asFragment()).toMatchSnapshot();
+});
